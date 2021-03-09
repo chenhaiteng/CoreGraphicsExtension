401d71e8b4d21e932324d221cc3e605d4fe9cc73
Chen-Hai Teng <chenhai.teng@positivegrid.com>
2021-03-09 16:12:06 +0800

    Remove lazy to make the public interface of CGPolarPoint be imutatable

--- Sources/CoreGraphicsExtension/CGPolarPoint.swift
+++ Sources/CoreGraphicsExtension/CGPolarPoint.swift
@@ -12,14 +12,16 @@ public struct CGPolarPoint {
     public static var zero = CGPolarPoint()
     public var radius: CGFloat
     public var cgangle: CGAngle
-    public lazy var cgpoint = CGPoint(x: radius*cos(cgangle.radians), y: radius*sin(cgangle.radians))
+    public var cgpoint: CGPoint
     @inlinable public init() {
         radius = 0
         cgangle = CGAngle.zero
+        cgpoint = CGPoint(x: radius*cos(cgangle.radians), y: radius*sin(cgangle.radians))
     }
     
     @inlinable public init<T: BinaryFloatingPoint>(radius: T, angle: CGAngle) {
         self.radius = CGFloat(radius)
         cgangle = angle
+        cgpoint = CGPoint(x: self.radius*cos(cgangle.radians), y: self.radius*sin(cgangle.radians))
     }
 }

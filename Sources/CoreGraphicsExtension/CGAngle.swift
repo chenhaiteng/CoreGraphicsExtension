d5e31f7b86b27d3df976183917d8099a6756d159
Chen-Hai Teng <chenhai.teng@positivegrid.com>
2021-03-11 16:11:53 +0800

    Remove redundant implementation of CGAngle. Use Type alias and extension to provide better implementation.

--- Sources/CoreGraphicsExtension/CGAngle.swift
+++ Sources/CoreGraphicsExtension/CGAngle.swift
@@ -8,45 +8,14 @@
 import Foundation
 import CoreGraphics
 
-public struct CGAngle {
-    public static var zero = CGAngle()
-    public var radians: CGFloat
-    
+public typealias CGAngle = CGFloat
+
+extension CGAngle {
+    public static var zero: CGFloat = 0.0
+    public var radians: CGFloat {
+        self
+    }
     public var degrees: CGFloat {
-        radians*180/CGFloat.pi
-    }
-
-    @inlinable public init() {
-        radians = 0
-    }
-
-    @inlinable public init<T: BinaryFloatingPoint>(radians: T) {
-        self.radians = CGFloat(radians)
-    }
-    
-    @inlinable public init<T: BinaryFloatingPoint>(degrees: T) {
-        self.radians = CGFloat(degrees)*CGFloat.pi/180
-    }
-    
-    @inlinable public static func radians<T: BinaryFloatingPoint>(_ radians: T) -> CGAngle {
-        CGAngle(radians: radians)
-    }
-    
-    @inlinable public static func degrees<T: BinaryFloatingPoint>(_ degrees: T) -> CGAngle {
-        CGAngle(degrees: degrees)
-    }
-    
-    public static func +(lhs:CGAngle, rhs: CGAngle) -> CGAngle {
-        CGAngle(radians: lhs.radians + rhs.radians)
-    }
-    
-    public static func +=(lhs: inout CGAngle, rhs: CGAngle) {
-        lhs.radians += rhs.radians
-    }
-}
-
-extension CGAngle: Hashable {
-    public func hash(into hasher: inout Hasher) {
-        hasher.combine(radians)
+        self*180/CGFloat.pi
     }
 }

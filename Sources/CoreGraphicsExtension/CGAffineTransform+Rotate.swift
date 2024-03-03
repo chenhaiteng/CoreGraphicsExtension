ed48d8db34a54008f72006eb29b3ec28337a0fed
Chen-Hai Teng <chenhaiteng@gmail.com>
2024-03-03 15:21:07 +0800

    fix typo

--- Sources/CoreGraphicsExtension/CGAffineTransform+Rotate.swift
+++ Sources/CoreGraphicsExtension/CGAffineTransform+Rotate.swift
@@ -10,7 +10,7 @@ import CoreGraphics
 
 public extension CGAffineTransform {
     /// Create a transform that rotate around the specified center with given angle.
-    static func rotateTransfrom(around center: CGPoint, by angle: CGAngle) -> CGAffineTransform {
+    static func rotateTransform(around center: CGPoint, by angle: CGAngle) -> CGAffineTransform {
         CGAffineTransform(translationX: center.x, y: center.y)
             .rotated(by: angle.radians)
             .translatedBy(x: -center.x, y: -center.y)
